0d976d40973b3f33614f14a2fcbba9bdd45126c5
Matthew Roxas <matthewroxas29@gmail.com>
2025-04-14 05:08:53 +0800

    now can be deployed

--- firebase.js
+++ firebase.js
@@ -1,10 +1,15 @@
 const admin = require("firebase-admin");
-require('dotenv').config();
+require('dotenv').config(); 
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+// const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG)
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
   databaseURL: "https://medicine-dispenser-38b4e-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
 
